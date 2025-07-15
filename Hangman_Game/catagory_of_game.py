68cb8b186f388c3414ffb7c7001e201f04739a21
Krishna Kishora K <krishnakishora04@gmail.com>
2025-07-15 22:52:42 +0530

    Add files via upload

---
+++ Hangman_Game/catagory_of_game.py
@@ -0,0 +1,30 @@
+words = {
+    "Animals": [
+        "Elephant", "Kangaroo", "Dolphin", "Cheetah", "Penguin",
+        "Octopus", "Crocodile", "Giraffe", "Squirrel", "lian"
+    ],
+    "Fruits & Vegetables": [
+        "Pineapple", "Strawberry", "Cucumber", "carrot", "Avocado",
+        "Blueberry", "Eggplant", "Pumpkin", "apple", "Watermelon"
+    ],
+    "Countries": [
+        "Australia", "Germany", "Portugal", "Canada", "Argentina",
+        "Thailand", "Indonesia", "india", "Switzerland", "japan"
+    ],
+    "Space & Science": [
+        "Astronaut", "Telescope", "Satellite", "Gravity", "Nebula",
+        "Meteorite", "Blackhole", "Supernova", "Oxygen", "Quantum"
+    ],
+    "Technology": [
+        "Algorithm", "Computer", "Internet", "Database", "Encryption",
+        "Firewall", "Processor", "Software", "Artificial", "Blockchain"
+    ],
+    "Mythology & Fantasy": [
+        "Unicorn", "dolphin", "Griffin", "cinema", "Krishna",
+        "mangalore", "Phoenix", "deathnote", "yagumilight"
+    ],
+    "Random Objects": [
+        "Notebook", "Backpack", "Headphone", "Sunglasses", "Umbrella",
+        "Bicycle", "Telescope", "Keyboard", "Airplane", "Compass"
+    ]
+}
